494f74bcefe5ebadeaca46164d6d21a10ea00fd5
Vadim Iakovlev <carnivorepotato@gmail.com>
2023-04-13 16:56:59 +0200

    i could not leave it like that so i refactored the code

---
+++ README.md
@@ -0,0 +1,7 @@
+# A small test project
+done in around 15-20 minutes
+
+## What is done:
+
+> - Checks if a text is an anagram of another text
+> - Unit tests
--- src/main/java/org/example/AnagramChecker.java
+++ src/main/java/org/example/AnagramChecker.java
@@ -3,25 +3,24 @@ package org.example;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class AnagramChecker {
 
     public boolean checkIfTextsAreAnagrams(String first, String second) {
-        if(trimAndCount(first) != trimAndCount(second)) {
+        if(getTrimmedLowercaseText(first).length() != getTrimmedLowercaseText(second).length()) {
             return false;
         }
-        Pair firstPair = trimAndCountEachCharacter(first);
-        Pair secondPair = trimAndCountEachCharacter(second);
+        Map<Character, Long> firstTextMap = countCharacters(first);
+        Map<Character, Long> secondTextMap = countCharacters(second);
 
-        return firstPair.equals(secondPair);
+        return firstTextMap.keySet().equals(secondTextMap.keySet()) &&
+                firstTextMap.entrySet().stream().allMatch(entry ->
+                entry.getValue().equals(secondTextMap.get(entry.getKey())));
     }
 
-    private Pair trimAndCountEachCharacter(String text) {
+    private Map<Character, Long> countCharacters(String text) {
         Map<Character, Long> resultMap = new HashMap<>();
-        String trimmedText = trim(text);
-        Set<Character> characterSet = trimmedText.chars().mapToObj(ch -> (char) ch).collect(Collectors.toSet());
+        String trimmedText = getTrimmedLowercaseText(text);
         List<Character> characterList = trimmedText.chars().mapToObj(ch -> (char) ch).toList();
 
         characterList.forEach(character -> {
@@ -29,22 +28,9 @@ public class AnagramChecker {
             resultMap.put(character, letterCount != null ? ++letterCount : 1L);
         });
 
-        return new Pair(characterSet, resultMap);
+        return resultMap;
     }
-
-    private int trimAndCount(String text) {
-        return trim(text).length();
-    }
-
-    private String trim(String text) {
+    private String getTrimmedLowercaseText(String text) {
         return text.trim().replaceAll("[^a-zA-Z]", "").toLowerCase();
-    }
-
-    private record Pair(Set<Character> left, Map<Character, Long> right) {
-
-        public boolean equals(Pair pair) {
-            return this.left.equals(pair.left()) && this.right().entrySet().stream().allMatch(entry ->
-                    entry.getValue().equals(pair.right().get(entry.getKey())));
-        }
     }
 }
